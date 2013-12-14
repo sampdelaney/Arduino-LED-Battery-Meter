23f150188860cc1141b1b78f49370d69f11f8a7c
Sam Delaney <samuelpeterdelaney@gmail.com>
2013-12-14 15:27:29 +0000

    Removed unused files and cleaned up some commented out functional code.

--- BatteryMeter.cpp
+++ BatteryMeter.cpp
@@ -5,10 +5,6 @@ BatteryMeter::BatteryMeter(float voltageMin, float voltageMax, Adc* adc)
 	_voltageMin = voltageMin;
 	_voltageMax = voltageMax;
 	_adc = adc;
-	
-	// Arduino 10-bit ADC
-	//_adcMax = pow(2, 10) - 1;
-	//_adcValPerVolts = (_adcVoltsMax - _adcVoltsMin) / _adcMax;
 }
 
 float BatteryMeter::getPercentage()
