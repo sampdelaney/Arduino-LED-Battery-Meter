cc19d60d6dfb0a55b346a9c8f52b57f1f55a8066
Sam Delaney <samuelpeterdelaney@gmail.com>
2013-12-15 10:02:23 +0000

    Corrected calculation for determining charge percentage.

--- BatteryMeter.cpp
+++ BatteryMeter.cpp
@@ -4,11 +4,12 @@ BatteryMeter::BatteryMeter(float voltageMin, float voltageMax, Adc* adc)
 {
 	_voltageMin = voltageMin;
 	_voltageMax = voltageMax;
+	_voltageRange = voltageMax - voltageMin;
 	_adc = adc;
 }
 
 float BatteryMeter::getPercentage()
 {
-	// determine percentage within min / max
-	return (_adc->readVolts() - _voltageMin) / _voltageMax;
+	// determine percentage within range
+	return (_adc->readVolts() - _voltageMin) / _voltageRange;
 }
